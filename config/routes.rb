b7347aa02af87d87b7d50535b368c2028f181c91
vitalik_vv <vitalikVVV@gmail.com>
2013-10-14 00:51:10 +0300

    Fixed routing

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,8 @@ TicketSysytem::Application.routes.draw do
   resources :tickets
 
 
-  resources :customers
-
-
+  get '/login', to: 'staff#login'
+  get '/register', to: 'staff#register'
 
 
   # The priority is based upon order of creation:
@@ -67,5 +66,5 @@ TicketSysytem::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  match ':controller(/:action(/:id))(.:format)'
 end
